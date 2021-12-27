6482d74a4f6b2f07c604f6d23f3165e6356d6938
Nuno Pereira <nunoafonso2002@gmail.com>
2021-12-27 18:41:04 +0000

    Refactored entity position logic

--- src/main/java/pt/up/fe/ldts/model/Entity.java
+++ src/main/java/pt/up/fe/ldts/model/Entity.java
@@ -5,9 +5,37 @@ package pt.up.fe.ldts.model;
  */
 public abstract class Entity {
 
-    private int x, y;
+    static class Position {
 
-    public Entity() {} // no need to do anything explicitly here, both int variables will start at 0
+        private int x, y;
+
+        public Position(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return this.x;
+        }
+
+        public int getY() {
+            return this.y;
+        }
+
+        private void setX(int newX) {
+            this.x = newX;
+        }
+
+        private void setY(int newY) {
+            this.y = newY;
+        }
+
+        public String toString() {
+            return "Pos[ x =" + this.x + ", y =" + this.y + "]";
+        }
+    }
+
+    private Position pos;
 
     /**
      * Constructs a new Entity on the given position
@@ -16,15 +44,27 @@ public abstract class Entity {
      * @param y the y coordinate of this entity
      */
     public Entity(int x, int y) {
-        this.x= x;
-        this.y = y;
+        this.pos = new Position(x, y);
+    }
+
+    public Position getPosition() {
+        return this.pos;
     }
 
     public int getX() {
-        return this.x;
+        return this.getPosition().getX();
     }
 
     public int getY() {
-        return this.y;
+        return this.getPosition().getY();
     }
+
+    public void changePos(int x, int y) {
+        var pos = this.getPosition();
+        pos.setX(x);
+        pos.setY(y);
+    }
+
+    public abstract void move(); // mark this as abstract so we can override it with different behaviours
+
 }
--- src/test/java/pt/up/fe/ldts/model/EntityTest.java
+++ src/test/java/pt/up/fe/ldts/model/EntityTest.java
@@ -7,10 +7,31 @@ public class EntityTest {
     
     @Test
     public void testCoords() {
-        Entity entity = new Entity(4, 5) {};
+
+        Entity entity = new Entity(4, 5) {
+            @Override
+            public void move() {}
+        };
 
         Assertions.assertEquals(entity.getX(), 4);
         Assertions.assertEquals(entity.getY(), 5);
+    }
+
+    @Test
+    public void testMove() {
+
+        Entity entity = new Entity(4, 5) {
+            @Override
+            public void move() {}
+        };
+
+        Assertions.assertEquals(entity.getX(), 4);
+        Assertions.assertEquals(entity.getY(), 5);
+
+        entity.changePos(10, 9);
+
+        Assertions.assertEquals(entity.getX(), 10);
+        Assertions.assertEquals(entity.getY(), 9);
     }
 
 }
