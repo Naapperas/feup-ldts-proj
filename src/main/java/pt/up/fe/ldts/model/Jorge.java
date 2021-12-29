ab3f422d85b330a431ddcb518c4f79a2c992ee36
Gonçalo Marques <goncalomarques.abacate@gmail.com>
2021-12-29 15:05:32 +0000

    moved the direction based methods over to entity and created abstract choose direction method

--- src/main/java/pt/up/fe/ldts/model/Jorge.java
+++ src/main/java/pt/up/fe/ldts/model/Jorge.java
@@ -2,35 +2,13 @@ package pt.up.fe.ldts.model;
 
 public class Jorge extends Entity{
 
-    enum Direction{
-        UP,
-        DOWN,
-        RIGHT,
-        LEFT
-    }
-
-    private Direction direction;
-
     public Jorge(int x, int y) {
         super(x, y);
-        direction = Direction.LEFT;
     }
 
-    public Direction getDirection() {
-        return direction;
-    }
-
-    public void setDirection(Direction direction) {
-        this.direction = direction;
-    }
-
-    public void move(){
-        switch (direction) {
-            case UP -> this.changePos(this.getX(), this.getY()-1);
-            case DOWN -> this.changePos(this.getX(), this.getY()+1);
-            case RIGHT -> this.changePos(this.getX()+1, this.getY());
-            case LEFT -> this.changePos(this.getX()-1, this.getY());
-        }
+    @Override
+    public void changeDirection() {
+        // change direction based on keystrokes
     }
 
 
--- src/test/java/pt/up/fe/ldts/model/EntityTest.java
+++ src/test/java/pt/up/fe/ldts/model/EntityTest.java
@@ -1,29 +1,31 @@
 package pt.up.fe.ldts.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class EntityTest {
+
+    private Entity entity;
+
+    @BeforeEach
+    public void setup(){
+        entity = new Entity(4, 5) {
+            @Override
+            public void changeDirection() {
+
+            }
+        };
+    }
     
     @Test
     public void testCoords() {
-
-        Entity entity = new Entity(4, 5) {
-            @Override
-            public void move() {}
-        };
-
         Assertions.assertEquals(4, entity.getX());
         Assertions.assertEquals(5, entity.getY());
     }
 
     @Test
-    public void testMove() {
-
-        Entity entity = new Entity(4, 5) {
-            @Override
-            public void move() {}
-        };
+    public void testChangePos() {
 
         Assertions.assertEquals(4, entity.getX());
         Assertions.assertEquals(5, entity.getY());
@@ -34,4 +36,50 @@ public class EntityTest {
         Assertions.assertEquals(9, entity.getY());
     }
 
+    @Test
+    public void testDirection() {
+
+        Assertions.assertEquals(Entity.Direction.LEFT, entity.getDirection());
+
+        // UP direction
+        entity.setDirection(Entity.Direction.UP);
+
+        Assertions.assertEquals(Entity.Direction.UP, entity.getDirection());
+
+        entity.move();
+
+        Assertions.assertEquals(4, entity.getX());
+        Assertions.assertEquals(4, entity.getY());
+
+        // DOWN direction
+        entity.setDirection(Entity.Direction.DOWN);
+
+        Assertions.assertEquals(Entity.Direction.DOWN, entity.getDirection());
+
+        entity.move();
+
+        Assertions.assertEquals(4, entity.getX());
+        Assertions.assertEquals(5, entity.getY());
+
+        // RIGHT
+        entity.setDirection(Entity.Direction.RIGHT);
+
+        Assertions.assertEquals(Entity.Direction.RIGHT, entity.getDirection());
+
+        entity.move();
+
+        Assertions.assertEquals(5, entity.getX());
+        Assertions.assertEquals(5, entity.getY());
+    }
+
+    @Test
+    public void testMove() {
+
+        Assertions.assertEquals(Entity.Direction.LEFT, entity.getDirection());
+
+        entity.move();
+
+        Assertions.assertEquals(3, entity.getX());
+        Assertions.assertEquals(5, entity.getY());
+    }
 }
