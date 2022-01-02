928d11e63995921ed708bdb6c5e54aeab199adb0
Gonçalo Marques <goncalomarques.abacate@gmail.com>
2022-01-02 14:47:53 +0000

    implemented zeCastroAI

--- src/main/java/pt/up/fe/ldts/controller/employeeAI/ZeCastroAI.java
+++ src/main/java/pt/up/fe/ldts/controller/employeeAI/ZeCastroAI.java
@@ -10,7 +10,7 @@ import java.util.Random;
 public class ZeCastroAI implements EmployeeAI{ //inky
 
     private Point baltaPos;
-    static final Point SCATTER_TARGET = new Point(0,20);// valores arbitrarios
+    static final Point SCATTER_TARGET = new Point(20,20);// valores arbitrarios
     static final Point DEAD_TARGET = new Point(10,10); // respawn
 
     public ZeCastroAI(Point baltaPos){
@@ -23,9 +23,19 @@ public class ZeCastroAI implements EmployeeAI{ //inky
     public Point chooseTargetPosition(Employee.EmployeeState state, Point position){
         return switch (state){
             case SCATTER -> SCATTER_TARGET;
-            case CHASING -> Vector.from(Jorge.singleton.getPosition(), position).magnitude() > 8 ? Jorge.singleton.getPosition() : SCATTER_TARGET;
+            case CHASING -> chaseTarget();
             case DEAD -> DEAD_TARGET;
             case FRIGHTENED -> new Point(random.nextInt(20), random.nextInt(20));
         };
+    }
+
+    private Point chaseTarget(){
+        Point target;
+        if(Jorge.singleton.getDirection().equals(Vector.UP))
+            target = Jorge.singleton.getPosition().addVector(Vector.UP.multiply(4)).addVector(Vector.LEFT.multiply(2));
+        else
+            target = Jorge.singleton.getPosition().addVector(Jorge.singleton.getDirection().multiply(2));
+
+        return target.addVector(Vector.from(baltaPos, target).multiply(-1));
     }
 }
