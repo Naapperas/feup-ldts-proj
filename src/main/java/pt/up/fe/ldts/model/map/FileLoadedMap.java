eb351de7f8cd2d305c2219731967865039507347
Gonçalo Marques <goncalomarques.abacate@gmail.com>
2022-01-11 22:55:00 +0000

    added fileLoadedMap implementation and tests

--- src/main/java/pt/up/fe/ldts/model/map/FileLoadedMap.java
+++ src/main/java/pt/up/fe/ldts/model/map/FileLoadedMap.java
@@ -1,24 +1,79 @@
 package pt.up.fe.ldts.model.map;
 
-import pt.up.fe.ldts.model.Collectible;
-import pt.up.fe.ldts.model.Employee;
-import pt.up.fe.ldts.model.Point;
-import pt.up.fe.ldts.model.Wall;
+import pt.up.fe.ldts.controller.employeeAI.BaltaAI;
+import pt.up.fe.ldts.controller.employeeAI.MariAI;
+import pt.up.fe.ldts.controller.employeeAI.ToniAI;
+import pt.up.fe.ldts.controller.employeeAI.ZeCastroAI;
+import pt.up.fe.ldts.model.*;
 
+import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileLoadedMap implements Map {
 
-    private int width, height;
+    private int width, height, boxWidth, boxHeight;
 
     private List<Wall> walls;
     private List<Employee> employees;
     private List<Collectible> collectibles;
 
-    private Point gatePosition;
+    private Point gatePosition, boxPosition; //top left corner of ghost box
 
-    public FileLoadedMap(String mapName) {
+    public FileLoadedMap(String mapName) throws Exception {
+        URL resource = getClass().getClassLoader().getResource("map/default.map");
+        File mapFile = new File(resource.toURI());
 
+        walls = new ArrayList<>();
+        employees = new ArrayList<>();
+        collectibles = new ArrayList<>();
+
+        BufferedReader br = new BufferedReader(new FileReader(mapFile));
+
+        String str = br.readLine();
+        String[] sr = str.split("X");
+
+        width = Integer.parseInt(sr[0]);
+        height = Integer.parseInt(sr[1]);
+
+        for(int y = 0; y < this.height; y++){
+            str = br.readLine();
+
+            if(str.length() != width){
+                StringBuilder sb = new StringBuilder();
+                sb.append("expected line with width: ").append(width).append(", instead got: ").append(str.length()).append(" - ").append(str);
+                throw new Exception(sb.toString());
+            }
+
+
+            for(int x = 0; x < this.width; x++){
+                switch (str.charAt(x)){
+                    case 'W' -> walls.add(new Wall(x,y));
+                    case 'T' -> collectibles.add(new Tremoco(x,y));
+                    case 'C' -> collectibles.add(new Cerveja(x,y));
+                    case 'J' -> Jorge.singleton.changePos(x,y);
+                    case 'G' -> gatePosition = new Point(x,y);
+                }
+            }
+        }
+
+        str = br.readLine();
+        sr = str.split(",");
+
+        boxPosition = new Point(Integer.parseInt(sr[0]), Integer.parseInt(sr[0]));
+
+        str = br.readLine();
+        sr = str.split("X");
+
+        boxWidth = Integer.parseInt(sr[0]);
+        boxHeight = Integer.parseInt(sr[1]);
+
+        employees.add(new Employee(13, 15, new BaltaAI()));
+        employees.add(new Employee(13, 16, new MariAI()));
+        employees.add(new Employee(14, 16, new ZeCastroAI(employees.get(0))));
+        employees.add(new Employee(12, 16, new ToniAI()));
     }
 
     @Override
@@ -50,4 +105,5 @@ public class FileLoadedMap implements Map {
     public int getHeight() {
         return this.height;
     }
+
 }
--- src/test/java/pt/up/fe/ldts/model/map/FileLoadedMapTest.java
+++ src/test/java/pt/up/fe/ldts/model/map/FileLoadedMapTest.java
@@ -1,5 +1,46 @@
 package pt.up.fe.ldts.model.map;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pt.up.fe.ldts.model.*;
+
+import java.util.List;
+
 public class FileLoadedMapTest {
+
+    private Map m;
+
+    @BeforeEach
+    void setup() throws Exception {
+        m = new FileLoadedMap("default");
+    }
+
+    @Test
+    public void testMapCreation(){
+
+        Assertions.assertEquals(27, m.getWidth());
+        Assertions.assertEquals(31, m.getHeight());
+
+        Assertions.assertTrue(m.getMapWalls().contains(new Wall(0,0)));
+        Assertions.assertTrue(m.getMapWalls().contains(new Wall(24,4)));
+
+        Assertions.assertTrue(m.getMapCollecibles().contains(new Cerveja(1,3)));
+        Assertions.assertTrue(m.getMapCollecibles().contains(new Cerveja(25,3)));
+
+        Assertions.assertTrue(m.getMapCollecibles().contains(new Tremoco(1,2)));
+        Assertions.assertTrue(m.getMapCollecibles().contains(new Tremoco(2,23)));
+
+        Assertions.assertEquals(new Point(13,23), Jorge.singleton.getPosition());
+
+        Assertions.assertEquals(new Point(13,12), m.getGatePosition());
+
+        var employees = m.getMapEmployees();
+
+        Assertions.assertNotNull(employees);
+        Assertions.assertEquals(4, employees.size());
+
+        employees.forEach(employee -> Assertions.assertEquals(Vector.UP, employee.getDirection()));
+    }
 
 }
