4e13c0cba041123142789d91ee63a2d667499839
Nuno Pereira <nunoafonso2002@gmail.com>
2022-01-14 06:41:35 +0000

    Added score tests

--- src/test/java/pt/up/fe/ldts/model/CollectibleTest.java
+++ src/test/java/pt/up/fe/ldts/model/CollectibleTest.java
@@ -6,4 +6,21 @@ import org.junit.jupiter.api.Test;
 
 public class CollectibleTest {
 
+    @Test
+    public void testPointsGiven() {
+
+        Tremoco t = new Tremoco(0, 0);
+        Cerveja c = new Cerveja(0, 0);
+
+        Assertions.assertEquals(0, Jorge.singleton.getScore());
+
+        t.notifyCollected();
+
+        Assertions.assertEquals(10, Jorge.singleton.getScore());
+
+        c.notifyCollected();
+
+        Assertions.assertEquals(30, Jorge.singleton.getScore());
+    }
+
 }
