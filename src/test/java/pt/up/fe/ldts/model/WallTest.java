7f9007e2aec3f802f572fa672eb7d6c2778d2440
Nuno Pereira <nunoafonso2002@gmail.com>
2022-01-18 03:40:59 +0000

    Added more tests

--- src/test/java/pt/up/fe/ldts/model/WallTest.java
+++ src/test/java/pt/up/fe/ldts/model/WallTest.java
@@ -5,5 +5,11 @@ import org.junit.jupiter.api.Test;
 
 public class WallTest {
 
+    @Test
+    public void testEquality() {
+        Wall w1 = new Wall(0, 0), w2 = new Wall(0, 1);
 
+        Assertions.assertFalse(w1.equals(null));
+        Assertions.assertTrue(w1.equals(w1));
+    }
 }
